d44b634a7e1c3f5abcdf33d30b4f5e0a86944e12
postaandras <91436854+postaandras@users.noreply.github.com>
2023-10-27 21:25:56 +0200

    Adding encryption as well as bootstrap
    Using bootstrap to give style to the error messages
    Adding one-way hashing

--- register_login/signup.php
+++ register_login/signup.php
@@ -14,12 +14,10 @@
             check_username($result, $user_name);
             check_password($password, $password2);
 
-            //save to database
-            
-            $query="insert into users (user_name, password) values ('$user_name', '$password')";
+            $encrypted = password_hash($password, PASSWORD_DEFAULT, ['cost'=>12]);
+            $query="insert into users (user_name, password) values ('$user_name', '$encrypted')";
             mysqli_query($con, $query);
             header("Location: signup.php");
-            
             die;
         }else{
             $_SESSION['status']="All fields must be filled in!";
@@ -34,14 +32,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign up</title>
     <link rel="stylesheet" href="style.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
 <body>
 
 <?php if(isset($_SESSION['status'])){ ?>
-            <?php echo $_SESSION['status'];
-            unset($_SESSION['status']);
-}?>
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <p class="warn">
+                        <strong>Warning!</strong> 
+                        <?php echo $_SESSION['status']; ?>
+                    </p>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+        <?php unset($_SESSION['status']);}?>
 
 
 <div id="box">
