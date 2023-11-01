b5cbbfa66d2fbb8bfbad558809387391a3d8f970
Bodnárová Nikoleta <bodnar.nikoleta.2002@gmail.com>
2023-11-01 14:32:22 +0100

    Added css style for indexpage

--- register_login/index.php
+++ register_login/index.php
@@ -10,6 +10,90 @@ session_start();
 <html>
 <head>
     <title>PenguinTODO</title>
+    <style>
+        body {
+            background-color: #007991;
+             }
+
+        header {
+            background-color:#222E50;
+            text-align: center;
+            padding: 20px 0px 10px 20px;
+            border-radius: 30px;
+        }
+
+        .header-main {
+            display: flex;
+        }
+
+        .header-icon {
+            width: 150px;
+            height: 150px;
+            margin-right: 10px;
+        }
+
+        .header-icon img {
+            width: 100px;
+            border-radius: 50%;
+        }
+
+        .header-title h1 {
+            font-size: 82px;
+            font-family: Impact;
+            color: #E9D985;
+            margin: 0;
+            text-decoration: overline;
+            padding-left: 20px;
+        }
+        .userinfo{
+            display:grid;
+            justify-content: left;
+        }
+        .logged-in h1{
+            font-size: 22px;
+            font-family:Georgia, 'Times New Roman', Times;
+            color: #E9D985;
+        }
+        .sign-out{
+            justify-self: right;
+        }
+        .sign-out a{
+            font-size: 15px;
+            font-family:Georgia, 'Times New Roman', Times;
+            color:#fff;
+        }
+
+        .container {
+            display: flex;
+            justify-content: space-evenly;
+            background-color: #007991;
+        }
+        .container h1{
+            color: #E9D985;
+            font-size: 42px;
+            font-family: Impact, fantasy;
+            font-weight: 100;
+        }
+
+        .button-container {
+            display: flex;
+            justify-content: space-evenly;
+            align-items: flex-start;
+            background-color: #007991;
+
+        }
+
+        .button a{
+            font-family: Impact, fantasy;
+            display: block;
+            background-color: #222E50;
+            color: #fff;
+            padding:50px 100px 50px 100px ;
+            text-decoration: none;
+            font-size: 150px;
+            border-radius: 30px;
+        }
+    </style>
 </head>
 <body>
     <header>
