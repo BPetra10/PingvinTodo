92575b881d6d2868bfe35373c2006efc6b72f3f2
Bodnárová Nikoleta <bodnar.nikoleta.2002@gmail.com>
2023-11-01 14:29:06 +0100

    Changed index page, added header

--- register_login/index.php
+++ register_login/index.php
@@ -1,20 +1,37 @@
 <?php
-    session_start();
-    include("connection.php");
-    include("functions.php");
-    check_login($con);
-?>
+session_start();
 
+    require("connection.php");
+    require("functions.php");
+    
+    $user_data=check_login($con);    
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PenguinTODO</title>
 </head>
 <body>
-    <h1> Hello! </h1>
-    <p>You are logged in as <?php echo $_SESSION['user_name'] ?></p>
-    <a href="logout.php">Sign out</a>
+    <header>
+        <div class="header-container">
+            <div class="header-main">
+                <div class="header-icon">
+                    <img src="https://i.ibb.co/YNppMLd/pingvin.png" alt="Penguin Button">
+                </div>
+                <div class="header-title">
+                    <h1>PenguinTODO</h1>
+                </div>
+            </div>
+            <div class ="userinfo">
+                <div class="logged-in">
+                    <h1>You are logged in as : <?php echo $_SESSION['user_name'];?></h1>
+
+                </div>
+                <div class="sign-out">
+                <a href="logout.php">You want to sign out?</a>
+                </div>
+        </div>
+        </div>
+    </header>
 </body>
 </html>
