6b6830278ef9587045527ecc9620a4b7e3b0ab1a
bpetra10 <bodnar.petra9@gmail.com>
2023-11-04 13:35:34 +0100

    Adding functions like mark as important, delete, checked elements etc.

--- todo/handleActions.php
+++ todo/handleActions.php
@@ -9,5 +9,22 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
             add_todos($_POST["task"]);
         }
     }
+    else if(isset($_POST["checked"]))
+    {
+        status_update($_POST["checked"]);
+    }
+    else if(isset($_POST["deleted"]))
+    {
+       delete_todos($_POST["deleted"]);
+    }
+    else if(isset($_POST["marked"]))
+    {
+        mark_as_important($_POST["marked"]);
+    }
+    else if(isset($_POST["marked_off"]))
+    {
+        unmark($_POST["marked_off"]);
+    }
+    header("Location:todo.php");
 }
 ?>
