977a255c3f688c27d496f2b86b91d11a962cc783
bpetra10 <bodnar.petra9@gmail.com>
2023-11-22 13:19:40 +0100

    Constant for header location fix

--- notes/actionHandler.php
+++ notes/actionHandler.php
@@ -1,19 +1,19 @@
 <?php
 
-
 require_once "functions.php";
 require_once "editModal.php";
 
+define("LOCATION", "Location: notes.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["addNote"])) {
         add_note($_POST["title"], $_POST["desc"]);
-        header("Location: notes.php");
+        header(LOCATION);
         die();
     } else if (isset($_POST["deleteNote"])) {
         $noteId = $_POST["noteId"];
         delete_note($noteId);
-        header("Location: notes.php");
+        header(LOCATION);
         die();
     } 
     else {
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $noteId = $_POST['hiddenInput'];
         
         change_note($edittitle, $editdesc, $noteId);
-        header("Location: notes.php");
+        header(LOCATION);
         die();
     } 
 };
